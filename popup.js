9376e79396663cae4567e189d5019a92615c50be
Kevin <kevtehpro35@gmail.com>
2016-05-15 02:07:21 -0400

    Added blocked site tracking

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
 var chill = document.querySelector(".chill"),
     work  = document.querySelector(".work");
-
 var interval;
 
 function update(data) {
-    display(data);
+    clearInterval(interval);
+
+    if (data.blocked != undefined && (data.blocked.indexOf(location.hostname) != -1 || data.blocked.indexOf(location.hostname + location.pathname) != -1)) {
+        document.querySelector(".block.selector").style.display = "none";
+    }
+    display(data.timeout);
     interval = setInterval(function() {
         display(data);
     }, 500);
 }
 
-function display(data) {
-    var timeLeft = getTimeLeft(data);
+function display(timeout) {
+    var timeLeft = getTimeLeft(timeout);
     if (timeLeft == "-1") {
         chill.style.display = "block";
         work.style.display  = "none";
@@ -23,13 +27,12 @@ function display(data) {
     document.querySelector(".timer").innerHTML = timeLeft;
 }
 
-function getTimeLeft(data) {
-    var timeout = data.timeout;
+function getTimeLeft(timeout) {
     var current = (new Date()).getTime();
 
     if (timeout != undefined && current <= parseInt(timeout)) {
         var delta = new Date(timeout - current);
-        var hours = (delta.getHours() < 10 ? "0" + delta.getHours() : delta.getHours()) - 19, 
+        var hours = (delta.getHours() - 19 < 10 ? "0" + (delta.getHours() - 19) : delta.getHours()) - 19, 
             mins  = delta.getMinutes() < 10 ? "0" + delta.getMinutes() : delta.getMinutes(),
             secs  = delta.getSeconds() < 10 ? "0" + delta.getSeconds() : delta.getSeconds();
         return hours + ":" + mins + ":" + secs;     
@@ -39,7 +42,7 @@ function getTimeLeft(data) {
 }
 
 function start() {
-    var selected = document.querySelector(".selected");
+    var selected = document.querySelector(".control.selector > .selected");
     var amount = selected.tagName == "A" ? selected.innerHTML : selected.value;
 
     var timeout = new Date((new Date()).getTime() + parseInt(amount) * 60000);
@@ -50,19 +53,43 @@ function start() {
     });
 }
 
-function select(el) {
-    document.querySelector(".selected").className = "";
+function select(selector, el) {
+    console.log(selector);
+    document.querySelector("." + selector + ".selector > .selected").className = "";
     el.className = "selected";
 }
 
 window.onload = function() {
-    var amount = document.querySelectorAll(".control > a,input");
-    for (var i = 0; i != amount.length; i++) {
-        amount[i].onclick = function() {
-            select(this);
+    var selector = document.querySelectorAll(".selector");
+    for (var j = 0; j != selector.length; j++) { 
+        var children = selector[j].querySelectorAll("a,input");
+        for (var i = 0; i != children.length; i++) {
+            children[i].onclick = function(s) {
+                return function() { 
+                    select(s.className.split(" ")[0], this);
+                }
+            }(selector[j]);
         }
     }
 
+    document.querySelector("button.block").onclick = function() {
+        chrome.storage.local.get("blocked", block);
+    }
+
     document.querySelector(".start").onclick = start;
-    chrome.storage.local.get("timeout", update);
+    chrome.storage.local.get(null, update);
 };
+
+function block(data) {
+    var blocked = data.blocked != undefined ? data.blocked : [];
+    switch (document.querySelector(".block.selector > .selected").innerHTML) {
+    case "Site":
+        blocked.push(location.hostname);
+        break;
+    case "Page":
+        blocked.push(location.hostname + location.pathname);
+        break;
+    }
+
+    chrome.storage.local.set({"blocked": blocked});
+}
