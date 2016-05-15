0693cf411e285a9ca5a9e7c0bbd015c2639fb691
Kevin <kevtehpro35@gmail.com>
2016-05-15 17:27:11 -0400

    Better block responsiveness

--- bkg.js
+++ bkg.js
@@ -1,24 +1,25 @@
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
-    if (req.method == "deny") {
-        chrome.storage.local.get(null, function(data) {
-            if (data.timeout == undefined || data.timeout <= (new Date()).getTime()) {
-                sendResponse({deny: false});
-                return;
-            }
+    if (req.method != "deny") 
+        return false;
 
-            if (data.blocked == undefined) {
-                sendResponse({deny: false});
-                return;
-            }
+    chrome.storage.local.get(null, function(data) {
+        if (data.timeout == undefined || data.timeout <= (new Date()).getTime()) {
+            sendResponse({deny: false});
+            return;
+        }
 
-            if (data.blocked.indexOf(req.loc.hostname) == -1 && data.blocked.indexOf(req.loc.hostname + req.loc.pathname) == -1) {
-                sendResponse({deny: false});
-                return;
-            }
+        if (data.blocked == undefined) {
+            sendResponse({deny: false});
+            return;
+        }
 
-            sendResponse({deny: true});
-        });
-        return true;
-    }
-    return false;
+        if (data.blocked.indexOf(req.loc.hostname) == -1 && data.blocked.indexOf(req.loc.hostname + req.loc.pathname) == -1) {
+            sendResponse({deny: false});
+            return;
+        }
+
+        sendResponse({deny: true});
+    });
+
+    return true;
 });
--- content.js
+++ content.js
@@ -1,9 +1,22 @@
 chrome.runtime.sendMessage({method: "deny", loc: location}, function(response) {
-    console.log(response);
     if (!response.deny) {
         return;
     }
 
-    var body = document.querySelector("body");
-    body.innerHTML = "";
+    document.open();
+
+    var dom = document.createElement("html");
+    var title = document.createElement("title");
+    title.innerHTML = "Nope";
+    dom.appendChild(title);
+
+    var warning = document.createElement("div");
+    warning.className = ".nope-warning";
+    warning.innerHTML = "What are you doing? Get back to work!";
+    warning.style.margin = "0 auto";
+    warning.style.height = "100%";
+    dom.appendChild(warning);
+
+    document.appendChild(dom);
+    document.close();
 });
